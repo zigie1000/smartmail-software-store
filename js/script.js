0c19de979fec395b00421eff370600055476ff18
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-07 13:03:25 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 jQuery(document).ready(function($) {
-    console.log("SmartMail Software Store script loaded.");
+    // Toggle software description on title click
+    $('.software-store-item-title').on('click', function() {
+        $(this).next('.software-store-item-description').slideToggle();
+    });
+
+    // Toggle ebook description on title click
+    $('.ebook-item-title').on('click', function() {
+        $(this).next('.ebook-item-description').slideToggle();
+    });
 });
