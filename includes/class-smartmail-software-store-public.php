0323d5192db78fb04714c87e32e2e685f9190891
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-07 11:25:26 +0200

    Update class-smartmail-software-store-public.php

--- includes/class-smartmail-software-store-public.php
+++ includes/class-smartmail-software-store-public.php
@@ -8,6 +8,7 @@ smartmail_log('Loading SmartMail_Software_Store_Public class');
 class SmartMail_Software_Store_Public {
     public function __construct() {
         smartmail_log('Constructing SmartMail_Software_Store_Public');
+        add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_public_scripts' ) );
     }
 
     public function enqueue_public_scripts() {
@@ -19,14 +20,14 @@ class SmartMail_Software_Store_Public {
     public function display_software_store() {
         smartmail_log('Displaying software store');
         ob_start();
-        include plugin_dir_path( __FILE__ ) . '../public-software.php';
+        include plugin_dir_path( dirname(__FILE__) ) . '../public-software.php';
         return ob_get_clean();
     }
 
     public function display_ebook_store() {
         smartmail_log('Displaying ebook store');
         ob_start();
-        include plugin_dir_path( __FILE__ ) . '../public-ebooks.php';
+        include plugin_dir_path( dirname(__FILE__) ) . '../public-ebooks.php';
         return ob_get_clean();
     }
 }
