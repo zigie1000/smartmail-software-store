9bb47d64e35e673c7478e693133748b4002bfe19
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-07 12:59:21 +0200

    Update public-software-.php

--- public-software-.php
+++ public-software-.php
@@ -1,19 +1,13 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) {
-    exit; // Exit if accessed directly.
-}
-
-echo '<div class="software-store-container">';
-echo '<h1 class="software-store-title">Software Store</h1>';
-echo '<div class="software-store-item">';
-echo '<div class="software-store-item-title">Example Software 1</div>';
-echo '<div class="software-store-item-price">$49.99</div>';
-echo '<div class="software-store-item-description" style="display: none;">This is a description of Example Software 1.</div>';
-echo '</div>';
-echo '<div class="software-store-item">';
-echo '<div class="software-store-item-title">Example Software 2</div>';
-echo '<div class="software-store-item-price">$29.99</div>';
-echo '<div class="software-store-item-description" style="display: none;">This is a description of Example Software 2.</div>';
-echo '</div>';
-echo '</div>';
-?>
+<div class="software-store-container">
+    <h1 class="software-store-title">Software Store</h1>
+    <div class="software-store-item">
+        <div class="software-store-item-title">Example Software 1</div>
+        <div class="software-store-item-price">$49.99</div>
+        <div class="software-store-item-description" style="display: none;">This is a description of Example Software 1.</div>
+    </div>
+    <div class="software-store-item">
+        <div class="software-store-item-title">Example Software 2</div>
+        <div class="software-store-item-price">$29.99</div>
+        <div class="software-store-item-description" style="display: none;">This is a description of Example Software 2.</div>
+    </div>
+</div>
