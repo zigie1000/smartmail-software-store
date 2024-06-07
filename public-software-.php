11aeb881acfc63d4e8c6b7d95cbd8cb2bf58c794
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-07 12:14:14 +0200

    Update public-software-.php

--- public-software-.php
+++ public-software-.php
@@ -8,12 +8,12 @@ echo '<h1 class="software-store-title">Software Store</h1>';
 echo '<div class="software-store-item">';
 echo '<div class="software-store-item-title">Example Software 1</div>';
 echo '<div class="software-store-item-price">$49.99</div>';
-echo '<div class="software-store-item-description" style="display: none;">This is a description of Example Software 1.</div>';
+echo '<div class="software-store-item-description">This is a description of Example Software 1.</div>';
 echo '</div>';
 echo '<div class="software-store-item">';
 echo '<div class="software-store-item-title">Example Software 2</div>';
 echo '<div class="software-store-item-price">$29.99</div>';
-echo '<div class="software-store-item-description" style="display: none;">This is a description of Example Software 2.</div>';
+echo '<div class="software-store-item-description">This is a description of Example Software 2.</div>';
 echo '</div>';
 echo '</div>';
 ?>
