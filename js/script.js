7987b1767529d1740e4410a5d66a9cbe4262817d
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-10 11:09:35 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,9 +3,4 @@ jQuery(document).ready(function($) {
     $('.software-store-item-title').on('click', function() {
         $(this).next('.software-store-item-description').slideToggle();
     });
-
-    // Toggle ebook description on title click
-    $('.ebook-item-title').on('click', function() {
-        $(this).next('.ebook-item-description').slideToggle();
-    });
 });
