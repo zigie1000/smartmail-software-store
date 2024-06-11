2bea7d3fa1f28376496e5d538a11434450c4c22a
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-11 20:12:08 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
+// JavaScript to handle the modal functionality
 jQuery(document).ready(function($) {
-    // Toggle software description on title click
-    $('.software-store-item-title').on('click', function() {
-        $(this).next('.software-store-item-description').slideToggle();
-    });
+    // Get the modal
+    var modal = document.getElementById("subscribeModal");
 
-    // Show subscription form
-    $('#subscribe-button').on('click', function() {
-        $('#subscribe-section').slideToggle();
-    });
+    // Get the button that opens the modal
+    var btn = document.getElementById("subscribeButton");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 });
