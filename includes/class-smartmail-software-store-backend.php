977fb473ec4a5e4735fc18391612ca2b9daa736f
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-18 00:53:23 +0200

    Create class-smartmail-software-store-backend.php

---
+++ includes/class-smartmail-software-store-backend.php
@@ -0,0 +1,26 @@
+<?php
+
+class SmartMail_Software_Store_Backend {
+    private $plugin_name;
+    private $version;
+
+    public function __construct($plugin_name, $version) {
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
+    }
+
+    public function add_plugin_backend_menu() {
+        add_submenu_page(
+            'smartmail-software-store',
+            'Backend Settings',
+            'Backend',
+            'manage_options',
+            'smartmail-software-store-backend',
+            array($this, 'display_plugin_backend_page')
+        );
+    }
+
+    public function display_plugin_backend_page() {
+        include_once plugin_dir_path(__FILE__) . '../templates/admin-backend-page.php';
+    }
+}
