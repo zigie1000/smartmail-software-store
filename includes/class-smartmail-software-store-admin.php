237713d225236990e77252e4cff40e702eee6d27
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-17 04:27:39 +0200

    Update class-smartmail-software-store-admin.php

--- includes/class-smartmail-software-store-admin.php
+++ includes/class-smartmail-software-store-admin.php
@@ -1,152 +1,68 @@
 <?php
 
-class SmartMail_Software_Store_Admin {
+class SmartMail_Software_Store_Public {
 
     public function __construct() {
-        add_action('admin_menu', array($this, 'register_admin_menu'));
-        add_action('admin_post_add_ebook', array($this, 'handle_ebook_submission'));
-        add_action('admin_post_add_software', array($this, 'handle_software_submission'));
-        add_action('admin_post_edit_product', array($this, 'handle_edit_submission'));
-        add_action('admin_post_delete_product', array($this, 'handle_delete_submission'));
+        add_shortcode('smartmail_store', array($this, 'display_store'));
     }
 
-    public function register_admin_menu() {
-        add_menu_page(
-            'SmartMail Software Store',
-            'SmartMail Store',
-            'manage_options',
-            'smartmail-software-store',
-            array($this, 'admin_ebooks_page'),
-            'dashicons-admin-generic',
-            6
-        );
+    public function display_store($atts) {
+        $atts = shortcode_atts(array(
+            'type' => 'ebook',
+        ), $atts, 'smartmail_store');
 
-        add_submenu_page(
-            'smartmail-software-store',
-            'eBooks',
-            'eBooks',
-            'manage_options',
-            'smartmail-software-store-ebooks',
-            array($this, 'admin_ebooks_page')
-        );
+        ob_start();
 
-        add_submenu_page(
-            'smartmail-software-store',
-            'Software',
-            'Software',
-            'manage_options',
-            'smartmail-software-store-software',
-            array($this, 'admin_software_page')
-        );
-    }
-
-    public function admin_ebooks_page() {
-        global $wpdb;
-        $ebooks_table_name = $wpdb->prefix . 'smartmail_ebooks';
-
-        $ebooks = $wpdb->get_results("SELECT * FROM $ebooks_table_name");
-
-        include plugin_dir_path(__FILE__) . '../templates/admin-ebooks-page.php';
-    }
-
-    public function admin_software_page() {
-        global $wpdb;
-        $software_table_name = $wpdb->prefix . 'smartmail_software';
-
-        $software = $wpdb->get_results("SELECT * FROM $software_table_name");
-
-        include plugin_dir_path(__FILE__) . '../templates/admin-software-page.php';
-    }
-
-    public function handle_ebook_submission() {
-        $this->handle_form_submission('ebook');
-    }
-
-    public function handle_software_submission() {
-        $this->handle_form_submission('software');
-    }
-
-    private function handle_form_submission($product_type) {
-        if (!current_user_can('manage_options')) {
-            return;
+        if ($atts['type'] == 'ebook') {
+            $this->display_ebooks();
+        } else {
+            $this->display_software();
         }
 
-        global $wpdb;
-        $table_name = $product_type == 'ebook' ? $wpdb->prefix . 'smartmail_ebooks' : $wpdb->prefix . 'smartmail_software';
-
-        if (isset($_POST['action']) && $_POST['action'] == "add_$product_type") {
-            $this->add_product($table_name);
-        } elseif (isset($_POST['action']) && $_POST['action'] == 'edit_product') {
-            $this->edit_product($table_name);
-        } elseif (isset($_POST['action']) && $_POST['action'] == 'delete_product') {
-            $this->delete_product($table_name);
-        }
-
-        $redirect_url = admin_url('admin.php?page=smartmail-software-store');
-        if ($product_type == 'software') {
-            $redirect_url = admin_url('admin.php?page=smartmail-software-store-software');
-        }
-        wp_redirect($redirect_url);
-        exit;
+        return ob_get_clean();
     }
 
-    private function add_product($table_name) {
-        global $wpdb;
-        $title = sanitize_text_field($_POST['title']);
-        $description = sanitize_textarea_field($_POST['description']);
-        $price = floatval($_POST['price']);
-        $rrp = floatval($_POST['rrp']);
-        $image_url = sanitize_text_field($_POST['image_url']);
-        $sku = sanitize_text_field($_POST['sku']);
-        $barcode = sanitize_text_field($_POST['barcode']);
-        $quantity = intval($_POST['quantity']);
-        $file_url = sanitize_text_field($_POST['file_url']);
-
-        $wpdb->insert($table_name, array(
-            'title' => $title,
-            'description' => $description,
-            'price' => $price,
-            'rrp' => $rrp,
-            'image_url' => $image_url,
-            'sku' => $sku,
-            'barcode' => $barcode,
-            'quantity' => $quantity,
-            'file_url' => $file_url
-        ));
+    private function display_ebooks() {
+        $args = array(
+            'post_type' => 'ebook',
+            'posts_per_page' => -1,
+        );
+        $ebooks = get_posts($args);
+        ?>
+        <div class="smartmail-store">
+            <h2>eBooks</h2>
+            <?php foreach ($ebooks as $ebook): ?>
+            <div class="ebook">
+                <h3><?php echo esc_html($ebook->post_title); ?></h3>
+                <p><?php echo esc_html($ebook->post_content); ?></p>
+                <p>Price: <?php echo esc_html(get_post_meta($ebook->ID, 'price', true)); ?></p>
+                <p>Quantity: <?php echo esc_html(get_post_meta($ebook->ID, 'quantity', true)); ?></p>
+                <a href="<?php echo esc_url(get_post_meta($ebook->ID, 'file_url', true)); ?>">Download</a>
+            </div>
+            <?php endforeach; ?>
+        </div>
+        <?php
     }
 
-    private function edit_product($table_name) {
-        global $wpdb;
-        $product_id = intval($_POST['product_id']);
-        $title = sanitize_text_field($_POST['title']);
-        $description = sanitize_textarea_field($_POST['description']);
-        $price = floatval($_POST['price']);
-        $rrp = floatval($_POST['rrp']);
-        $image_url = sanitize_text_field($_POST['image_url']);
-        $sku = sanitize_text_field($_POST['sku']);
-        $barcode = sanitize_text_field($_POST['barcode']);
-        $quantity = intval($_POST['quantity']);
-        $file_url = sanitize_text_field($_POST['file_url']);
-
-        $wpdb->update($table_name, array(
-            'title' => $title,
-            'description' => $description,
-            'price' => $price,
-            'rrp' => $rrp,
-            'image_url' => $image_url,
-            'sku' => $sku,
-            'barcode' => $barcode,
-            'quantity' => $quantity,
-            'file_url' => $file_url
-        ), array('id' => $product_id));
-    }
-
-    private function delete_product($table_name) {
-        global $wpdb;
-        $product_id = intval($_POST['product_id']);
-
-        $wpdb->delete($table_name, array('id' => $product_id));
+    private function display_software() {
+        $args = array(
+            'post_type' => 'software',
+            'posts_per_page' => -1,
+        );
+        $software = get_posts($args);
+        ?>
+        <div class="smartmail-store">
+            <h2>Software</h2>
+            <?php foreach ($software as $item): ?>
+            <div class="software">
+                <h3><?php echo esc_html($item->post_title); ?></h3>
+                <p><?php echo esc_html($item->post_content); ?></p>
+                <p>Price: <?php echo esc_html(get_post_meta($item->ID, 'price', true)); ?></p>
+                <p>Quantity: <?php echo esc_html(get_post_meta($item->ID, 'quantity', true)); ?></p>
+                <a href="<?php echo esc_url(get_post_meta($item->ID, 'file_url', true)); ?>">Download</a>
+            </div>
+            <?php endforeach; ?>
+        </div>
+        <?php
     }
 }
-
-new SmartMail_Software_Store_Admin();
