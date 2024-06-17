a14719ebc4def911a4ea4b4d0fc6252ca13afa25
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-17 21:52:52 +0200

    Update class-smartmail-software-store-public.php

--- includes/class-smartmail-software-store-public.php
+++ includes/class-smartmail-software-store-public.php
@@ -1,13 +1,4 @@
 <?php
-/**
- * The public-facing functionality of the plugin.
- *
- * @link       https://smartmail.store
- * @since      1.0.0
- *
- * @package    SmartMail_Software_Store
- * @subpackage SmartMail_Software_Store/public
- */
 
 class SmartMail_Software_Store_Public {
     private $plugin_name;
@@ -17,13 +8,4 @@ class SmartMail_Software_Store_Public {
         $this->plugin_name = $plugin_name;
         $this->version = $version;
     }
-
-    public function enqueue_styles() {
-        wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/smartmail-software-store-public.css', array(), $this->version, 'all');
-    }
-
-    public function enqueue_scripts() {
-        wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/smartmail-software-store-public.js', array('jquery'), $this->version, false);
-    }
 }
-?>
