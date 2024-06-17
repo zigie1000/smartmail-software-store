e174212d9917a6b84cd37948b7ed3f77ade2bcb3
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-17 23:46:51 +0200

    Update class-smartmail-software-store-public.php

--- includes/class-smartmail-software-store-public.php
+++ includes/class-smartmail-software-store-public.php
@@ -1,29 +1,37 @@
 <?php
 
-/**
- * The public-facing functionality of the plugin.
- *
- * @link       https://smartmail.store
- * @since      1.0.0
- *
- * @package    SmartMail_Software_Store
- * @subpackage SmartMail_Software_Store/public
- */
-class SmartMail_Software_Store_Public {
+class SmartMail_Software_Store_Admin {
+
     private $plugin_name;
     private $version;
 
-    public function __construct($plugin_name, $version) {
+    public function __construct( $plugin_name, $version ) {
         $this->plugin_name = $plugin_name;
         $this->version = $version;
     }
 
     public function enqueue_styles() {
-        wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/smartmail-software-store-public.css', array(), $this->version, 'all');
+        wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/smartmail-software-store-admin.css', array(), $this->version, 'all' );
     }
 
     public function enqueue_scripts() {
-        wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/smartmail-software-store-public.js', array('jquery'), $this->version, false);
+        wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/smartmail-software-store-admin.js', array( 'jquery' ), $this->version, false );
+    }
+
+    public function add_plugin_admin_menu() {
+        add_menu_page(
+            'SmartMail Software Store',
+            'SmartMail Store',
+            'manage_options',
+            $this->plugin_name,
+            array( $this, 'display_plugin_admin_page' ),
+            'dashicons-store',
+            26
+        );
+    }
+
+    public function display_plugin_admin_page() {
+        include_once plugin_dir_path(__FILE__) . 'partials/smartmail-software-store-admin-display.php';
     }
 }
 ?>
