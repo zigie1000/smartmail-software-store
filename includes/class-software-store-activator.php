56aa7b88c5b80680655a44163527e99480f0a29d
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-17 23:51:48 +0200

    Update class-software-store-activator.php

--- includes/class-software-store-activator.php
+++ includes/class-software-store-activator.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * Fired during plugin activation.
- *
- * @link       https://smartmail.store
- * @since      1.0.0
- *
- * @package    SmartMail_Software_Store
- * @subpackage SmartMail_Software_Store/includes
- */
 class SmartMail_Software_Store_Activator {
 
     public static function activate() {
@@ -17,22 +8,21 @@ class SmartMail_Software_Store_Activator {
     }
 
     private static function create_custom_post_types() {
-        // Register eBook post type
         $ebook_labels = array(
-            'name'               => _x('eBooks', 'post type general name', 'smartmail-software-store'),
-            'singular_name'      => _x('eBook', 'post type singular name', 'smartmail-software-store'),
-            'menu_name'          => _x('eBooks', 'admin menu', 'smartmail-software-store'),
-            'name_admin_bar'     => _x('eBook', 'add new on admin bar', 'smartmail-software-store'),
-            'add_new'            => _x('Add New', 'eBook', 'smartmail-software-store'),
-            'add_new_item'       => __('Add New eBook', 'smartmail-software-store'),
-            'new_item'           => __('New eBook', 'smartmail-software-store'),
-            'edit_item'          => __('Edit eBook', 'smartmail-software-store'),
-            'view_item'          => __('View eBook', 'smartmail-software-store'),
-            'all_items'          => __('All eBooks', 'smartmail-software-store'),
-            'search_items'       => __('Search eBooks', 'smartmail-software-store'),
-            'parent_item_colon'  => __('Parent eBooks:', 'smartmail-software-store'),
-            'not_found'          => __('No eBooks found.', 'smartmail-software-store'),
-            'not_found_in_trash' => __('No eBooks found in Trash.', 'smartmail-software-store')
+            'name'               => _x( 'eBooks', 'post type general name', 'smartmail-software-store' ),
+            'singular_name'      => _x( 'eBook', 'post type singular name', 'smartmail-software-store' ),
+            'menu_name'          => _x( 'eBooks', 'admin menu', 'smartmail-software-store' ),
+            'name_admin_bar'     => _x( 'eBook', 'add new on admin bar', 'smartmail-software-store' ),
+            'add_new'            => _x( 'Add New', 'eBook', 'smartmail-software-store' ),
+            'add_new_item'       => __( 'Add New eBook', 'smartmail-software-store' ),
+            'new_item'           => __( 'New eBook', 'smartmail-software-store' ),
+            'edit_item'          => __( 'Edit eBook', 'smartmail-software-store' ),
+            'view_item'          => __( 'View eBook', 'smartmail-software-store' ),
+            'all_items'          => __( 'All eBooks', 'smartmail-software-store' ),
+            'search_items'       => __( 'Search eBooks', 'smartmail-software-store' ),
+            'parent_item_colon'  => __( 'Parent eBooks:', 'smartmail-software-store' ),
+            'not_found'          => __( 'No eBooks found.', 'smartmail-software-store' ),
+            'not_found_in_trash' => __( 'No eBooks found in Trash.', 'smartmail-software-store' )
         );
 
         $ebook_args = array(
@@ -42,32 +32,31 @@ class SmartMail_Software_Store_Activator {
             'show_ui'            => true,
             'show_in_menu'       => true,
             'query_var'          => true,
-            'rewrite'            => array('slug' => 'ebook'),
+            'rewrite'            => array( 'slug' => 'ebook' ),
             'capability_type'    => 'post',
             'has_archive'        => true,
             'hierarchical'       => false,
             'menu_position'      => null,
-            'supports'           => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'),
+            'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
         );
 
-        register_post_type('ebook', $ebook_args);
+        register_post_type( 'ebook', $ebook_args );
 
-        // Register Software post type
         $software_labels = array(
-            'name'               => _x('Software', 'post type general name', 'smartmail-software-store'),
-            'singular_name'      => _x('Software', 'post type singular name', 'smartmail-software-store'),
-            'menu_name'          => _x('Software', 'admin menu', 'smartmail-software-store'),
-            'name_admin_bar'     => _x('Software', 'add new on admin bar', 'smartmail-software-store'),
-            'add_new'            => _x('Add New', 'Software', 'smartmail-software-store'),
-            'add_new_item'       => __('Add New Software', 'smartmail-software-store'),
-            'new_item'           => __('New Software', 'smartmail-software-store'),
-            'edit_item'          => __('Edit Software', 'smartmail-software-store'),
-            'view_item'          => __('View Software', 'smartmail-software-store'),
-            'all_items'          => __('All Software', 'smartmail-software-store'),
-            'search_items'       => __('Search Software', 'smartmail-software-store'),
-            'parent_item_colon'  => __('Parent Software:', 'smartmail-software-store'),
-            'not_found'          => __('No Software found.', 'smartmail-software-store'),
-            'not_found_in_trash' => __('No Software found in Trash.', 'smartmail-software-store')
+            'name'               => _x( 'Software', 'post type general name', 'smartmail-software-store' ),
+            'singular_name'      => _x( 'Software', 'post type singular name', 'smartmail-software-store' ),
+            'menu_name'          => _x( 'Software', 'admin menu', 'smartmail-software-store' ),
+            'name_admin_bar'     => _x( 'Software', 'add new on admin bar', 'smartmail-software-store' ),
+            'add_new'            => _x( 'Add New', 'Software', 'smartmail-software-store' ),
+            'add_new_item'       => __( 'Add New Software', 'smartmail-software-store' ),
+            'new_item'           => __( 'New Software', 'smartmail-software-store' ),
+            'edit_item'          => __( 'Edit Software', 'smartmail-software-store' ),
+            'view_item'          => __( 'View Software', 'smartmail-software-store' ),
+            'all_items'          => __( 'All Software', 'smartmail-software-store' ),
+            'search_items'       => __( 'Search Software', 'smartmail-software-store' ),
+            'parent_item_colon'  => __( 'Parent Software:', 'smartmail-software-store' ),
+            'not_found'          => __( 'No Software found.', 'smartmail-software-store' ),
+            'not_found_in_trash' => __( 'No Software found in Trash.', 'smartmail-software-store' )
         );
 
         $software_args = array(
@@ -77,15 +66,15 @@ class SmartMail_Software_Store_Activator {
             'show_ui'            => true,
             'show_in_menu'       => true,
             'query_var'          => true,
-            'rewrite'            => array('slug' => 'software'),
+            'rewrite'            => array( 'slug' => 'software' ),
             'capability_type'    => 'post',
             'has_archive'        => true,
             'hierarchical'       => false,
             'menu_position'      => null,
-            'supports'           => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'),
+            'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
         );
 
-        register_post_type('software', $software_args);
+        register_post_type( 'software', $software_args );
     }
 }
 ?>
