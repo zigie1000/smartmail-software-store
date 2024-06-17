33779d8b7dffc87ac7b1b5789a90cde65998283f
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-17 16:50:28 +0200

    Update smartmail-software-store.php

--- smartmail-software-store.php
+++ smartmail-software-store.php
@@ -1,36 +1,38 @@
 <?php
 /**
  * Plugin Name: SmartMail Software Store
- * Plugin URI: https://smartmail.store
+ * Plugin URI:  https://smartmail.store
  * Description: A plugin to manage and display eBooks and Software.
- * Version: 1.0.0
- * Author: Marco Zagato
- * Author URI: https://smartmail.store
- * License: GPL2
+ * Version:     1.0.0
+ * Author:      Marco Zagato
+ * Author URI:  https://smartmail.store
+ * Text Domain: smartmail-software-store
+ * Domain Path: /languages
  */
 
 if (!defined('WPINC')) {
     die;
 }
 
-require_once plugin_dir_path(__FILE__) . 'includes/class-smartmail-software-store-activator.php';
-require_once plugin_dir_path(__FILE__) . 'includes/class-smartmail-software-store-deactivator.php';
-require_once plugin_dir_path(__FILE__) . 'includes/class-smartmail-software-store-admin.php';
-require_once plugin_dir_path(__FILE__) . 'includes/class-smartmail-software-store-public.php';
+define('SMARTMAIL_SOFTWARE_STORE_VERSION', '1.0.0');
 
 function activate_smartmail_software_store() {
+    require_once plugin_dir_path(__FILE__) . 'includes/class-smartmail-software-store-activator.php';
     SmartMail_Software_Store_Activator::activate();
 }
 
 function deactivate_smartmail_software_store() {
+    require_once plugin_dir_path(__FILE__) . 'includes/class-smartmail-software-store-deactivator.php';
     SmartMail_Software_Store_Deactivator::deactivate();
 }
 
 register_activation_hook(__FILE__, 'activate_smartmail_software_store');
 register_deactivation_hook(__FILE__, 'deactivate_smartmail_software_store');
 
+require plugin_dir_path(__FILE__) . 'includes/class-smartmail-software-store.php';
+
 function run_smartmail_software_store() {
-    $plugin = new SmartMail_Software_Store_Admin('smartmail-software-store', '1.0.0');
+    $plugin = new SmartMail_Software_Store();
     $plugin->run();
 }
 
