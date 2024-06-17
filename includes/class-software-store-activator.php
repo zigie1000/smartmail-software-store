54861fee8f27cdbe16f1dc7100d38e903c94a135
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-17 21:50:40 +0200

    Update class-software-store-activator.php

--- includes/class-software-store-activator.php
+++ includes/class-software-store-activator.php
@@ -1,16 +1,8 @@
 <?php
-/**
- * Fired during plugin activation
- *
- * @link       https://smartmail.store
- * @since      1.0.0
- *
- * @package    SmartMail_Software_Store
- * @subpackage SmartMail_Software_Store/includes
- */
 
 class SmartMail_Software_Store_Activator {
     public static function activate() {
+        // Create custom post types for eBooks and Software
         self::create_custom_post_types();
     }
 
@@ -86,4 +78,3 @@ class SmartMail_Software_Store_Activator {
         register_post_type('software', $software_args);
     }
 }
-?>
