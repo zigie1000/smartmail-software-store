d110ff76c5924183311b6ba6a85e081205c6b4ac
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-17 21:52:11 +0200

    Update class-smartmail-software-store-admin.php

--- includes/class-smartmail-software-store-admin.php
+++ includes/class-smartmail-software-store-admin.php
@@ -1,13 +1,4 @@
 <?php
-/**
- * The admin-specific functionality of the plugin.
- *
- * @link       https://smartmail.store
- * @since      1.0.0
- *
- * @package    SmartMail_Software_Store
- * @subpackage SmartMail_Software_Store/admin
- */
 
 class SmartMail_Software_Store_Admin {
     private $plugin_name;
@@ -18,14 +9,6 @@ class SmartMail_Software_Store_Admin {
         $this->version = $version;
     }
 
-    public function enqueue_styles() {
-        wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/smartmail-software-store-admin.css', array(), $this->version, 'all');
-    }
-
-    public function enqueue_scripts() {
-        wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/smartmail-software-store-admin.js', array('jquery'), $this->version, false);
-    }
-
     public function add_plugin_admin_menu() {
         add_menu_page(
             'SmartMail Software Store',
@@ -33,21 +16,24 @@ class SmartMail_Software_Store_Admin {
             'manage_options',
             $this->plugin_name,
             array($this, 'display_plugin_admin_page'),
-            'dashicons-store',
+            'dashicons-admin-generic',
             26
+        );
+        add_submenu_page(
+            $this->plugin_name,
+            'SmartMail Software Store Settings',
+            'Settings',
+            'manage_options',
+            $this->plugin_name . '-settings',
+            array($this, 'display_plugin_settings_page')
         );
     }
 
     public function display_plugin_admin_page() {
-        include_once 'templates/admin-page.php';
+        include_once plugin_dir_path(__FILE__) . '../templates/admin-page.php';
     }
 
-    public function display_ebook_admin_page() {
-        include_once 'templates/admin-ebooks-page.php';
-    }
-
-    public function display_software_admin_page() {
-        include_once 'templates/admin-software-page.php';
+    public function display_plugin_settings_page() {
+        include_once plugin_dir_path(__FILE__) . '../templates/admin-settings-page.php';
     }
 }
-?>
