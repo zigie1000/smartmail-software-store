67cf4572d2ce564950be878bfdd761a0836c279b
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-17 22:20:39 +0200

    Update class-smartmail-software-store-public.php

--- includes/class-smartmail-software-store-public.php
+++ includes/class-smartmail-software-store-public.php
@@ -1,11 +1,17 @@
 <?php
 
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * @link       https://smartmail.store
+ * @since      1.0.0
+ *
+ * @package    SmartMail_Software_Store
+ * @subpackage SmartMail_Software_Store/public
+ */
 class SmartMail_Software_Store_Public {
     private $plugin_name;
     private $version;
 
     public function __construct($plugin_name, $version) {
-        $this->plugin_name = $plugin_name;
-        $this->version = $version;
-    }
-}
+       
