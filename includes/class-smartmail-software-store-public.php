a1fb46d073470886d638cba579cfd139ff9e8aa1
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-17 13:38:06 +0200

    Update class-smartmail-software-store-public.php

--- includes/class-smartmail-software-store-public.php
+++ includes/class-smartmail-software-store-public.php
@@ -1,29 +1,26 @@
 <?php
+
 /**
  * SmartMail Software Store Public Class
- * 
- * @package SmartMail_Software_Store
+ *
+ * @package SmartMail Software Store
  * @author Marco Zagato
- * @author URI: https://smartmail.store
+ * @author URI https://smartmail.store
  */
 
 class SmartMail_Software_Store_Public {
-
     public function __construct() {
-        // Hook into WordPress
         add_action('wp_enqueue_scripts', array($this, 'enqueue_styles'));
         add_action('wp_enqueue_scripts', array($this, 'enqueue_scripts'));
     }
 
     public function enqueue_styles() {
-        wp_enqueue_style('smartmail-software-store-public-css', plugin_dir_url(__FILE__) . 'css/style.css');
+        wp_enqueue_style('smartmail-software-store', plugin_dir_url(__FILE__) . 'css/style.css', array(), '1.0.0', 'all');
     }
 
     public function enqueue_scripts() {
-        wp_enqueue_script('smartmail-software-store-public-js', plugin_dir_url(__FILE__) . 'js/script.js');
+        wp_enqueue_script('smartmail-software-store', plugin_dir_url(__FILE__) . 'js/script.js', array('jquery'), '1.0.0', true);
     }
 }
 
-if (class_exists('SmartMail_Software_Store_Public')) {
-    $smartmail_software_store_public = new SmartMail_Software_Store_Public();
-}
+new SmartMail_Software_Store_Public();
