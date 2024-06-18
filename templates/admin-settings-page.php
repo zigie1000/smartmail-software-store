369c24f61f49f5ff7d336fc988cb44000cb130d8
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-18 12:27:45 +0200

    Update admin-settings-page.php

--- templates/admin-settings-page.php
+++ templates/admin-settings-page.php
@@ -1,20 +1,16 @@
-<div class="wrap">
-    <h1><?php _e('Settings', 'smartmail-software-store'); ?></h1>
-    <form method="post" action="options.php">
-        <?php
-        settings_fields('smartmail_store_settings_group');
-        do_settings_sections('smartmail_store_settings_group');
-        ?>
-        <table class="form-table">
-            <tr valign="top">
-                <th scope="row"><?php _e('Setting 1', 'smartmail-software-store'); ?></th>
-                <td><input type="text" name="smartmail_store_setting1" value="<?php echo esc_attr(get_option('smartmail_store_setting1')); ?>" /></td>
-            </tr>
-            <tr valign="top">
-                <th scope="row"><?php _e('Setting 2', 'smartmail-software-store'); ?></th>
-                <td><input type="text" name="smartmail_store_setting2" value="<?php echo esc_attr(get_option('smartmail_store_setting2')); ?>" /></td>
-            </tr>
-        </table>
-        <?php submit_button(); ?>
-    </form>
-</div>
+<h1>SmartMail Store Settings</h1>
+<form method="post" action="options.php">
+    <?php settings_fields('smartmail_store_settings_group'); ?>
+    <?php do_settings_sections('smartmail_store_settings_group'); ?>
+    <table class="form-table">
+        <tr valign="top">
+            <th scope="row">Setting 1</th>
+            <td><input type="text" name="smartmail_store_setting1" value="<?php echo esc_attr(get_option('smartmail_store_setting1')); ?>" /></td>
+        </tr>
+        <tr valign="top">
+            <th scope="row">Setting 2</th>
+            <td><input type="text" name="smartmail_store_setting2" value="<?php echo esc_attr(get_option('smartmail_store_setting2')); ?>" /></td>
+        </tr>
+    </table>
+    <?php submit_button(); ?>
+</form>
