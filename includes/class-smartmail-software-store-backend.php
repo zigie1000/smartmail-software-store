846174be05cc856ec062b2be1ebe16e4824c74c3
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-18 02:22:52 +0200

    Update class-smartmail-software-store-backend.php

--- includes/class-smartmail-software-store-backend.php
+++ includes/class-smartmail-software-store-backend.php
@@ -12,32 +12,17 @@ class SmartMail_Software_Store_Backend {
     public function add_plugin_backend_menu() {
         add_menu_page(
             'SmartMail Software Store Backend',
-            'SmartMail Backend',
+            'SmartMail Store Backend',
             'manage_options',
-            $this->plugin_name . '-backend',
+            'smartmail-software-store-backend',
             array($this, 'display_plugin_backend_page'),
-            'dashicons-admin-generic',
-            27
+            'dashicons-store',
+            26
         );
     }
 
     public function display_plugin_backend_page() {
-        ?>
-        <div class="wrap">
-            <h1>SmartMail Software Store Backend</h1>
-            <form method="post" action="options.php">
-                <?php settings_fields('smartmail-software-store-backend-group'); ?>
-                <?php do_settings_sections('smartmail-software-store-backend-group'); ?>
-                <table class="form-table">
-                    <tr valign="top">
-                    <th scope="row">Backend field description</th>
-                    <td><input type="text" name="smartmail_software_store_backend_field" value="<?php echo esc_attr(get_option('smartmail_software_store_backend_field')); ?>" /></td>
-                    </tr>
-                </table>
-                <?php submit_button(); ?>
-            </form>
-        </div>
-        <?php
+        include_once('templates/admin-backend-page.php');
     }
 }
 ?>
