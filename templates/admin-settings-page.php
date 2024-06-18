04b4ba0e92591d788a1612e1b14a932caa8e23d6
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-18 12:52:56 +0200

    Update admin-settings-page.php

--- templates/admin-settings-page.php
+++ templates/admin-settings-page.php
@@ -1,16 +1,24 @@
-<h1>SmartMail Store Settings</h1>
-<form method="post" action="options.php">
-    <?php settings_fields('smartmail_store_settings_group'); ?>
-    <?php do_settings_sections('smartmail_store_settings_group'); ?>
-    <table class="form-table">
-        <tr valign="top">
-            <th scope="row">Setting 1</th>
-            <td><input type="text" name="smartmail_store_setting1" value="<?php echo esc_attr(get_option('smartmail_store_setting1')); ?>" /></td>
-        </tr>
-        <tr valign="top">
-            <th scope="row">Setting 2</th>
-            <td><input type="text" name="smartmail_store_setting2" value="<?php echo esc_attr(get_option('smartmail_store_setting2')); ?>" /></td>
-        </tr>
-    </table>
-    <?php submit_button(); ?>
-</form>
+<div class="wrap">
+    <h1>SmartMail Store Settings</h1>
+    <form method="post" action="options.php">
+        <?php
+        settings_fields('smartmail_store_settings_group');
+        do_settings_sections('smartmail_store_settings_group');
+        ?>
+        <table class="form-table">
+            <tr valign="top">
+                <th scope="row">Logo URL</th>
+                <td><input type="text" name="smartmail_store_logo" value="<?php echo esc_attr(get_option('smartmail_store_logo')); ?>" /></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">Allowed eBook Types</th>
+                <td><input type="text" name="smartmail_store_ebook_types" value="<?php echo esc_attr(get_option('smartmail_store_ebook_types')); ?>" /></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">Allowed Software Types</th>
+                <td><input type="text" name="smartmail_store_software_types" value="<?php echo esc_attr(get_option('smartmail_store_software_types')); ?>" /></td>
+            </tr>
+        </table>
+        <?php submit_button(); ?>
+    </form>
+</div>
