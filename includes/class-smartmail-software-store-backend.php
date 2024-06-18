0c5597973d2847b1b7ba54019f872c989ef67630
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-18 08:13:01 +0200

    Update class-smartmail-software-store-backend.php

--- includes/class-smartmail-software-store-backend.php
+++ includes/class-smartmail-software-store-backend.php
@@ -1,58 +1,20 @@
 <?php
-/**
- * Backend class for SmartMail Software Store Plugin
- * 
- * Handles the backend functionalities.
- */
+
 class SmartMail_Software_Store_Backend {
-    private $plugin_name;
-    private $version;
 
-    public function __construct($plugin_name, $version) {
-        $this->plugin_name = $plugin_name;
-        $this->version = $version;
-    }
-
-    public function add_plugin_backend_menu() {
+    public static function add_plugin_backend_menu() {
         add_menu_page(
+            __('SmartMail Software Store Backend', 'smartmail-software-store'),
             __('SmartMail Store Backend', 'smartmail-software-store'),
-            __('SmartMail Store Backend', 'smartmail-software-store'),
             'manage_options',
-            $this->plugin_name . '-backend',
-            array($this, 'display_plugin_backend_dashboard'),
-            'dashicons-admin-tools',
-            26
-        );
-
-        add_submenu_page(
-            $this->plugin_name . '-backend',
-            __('Add/Edit/Delete eBooks', 'smartmail-software-store'),
-            __('Ebooks', 'smartmail-software-store'),
-            'manage_options',
-            $this->plugin_name . '-backend-ebooks',
-            array($this, 'display_plugin_backend_ebooks')
-        );
-
-        add_submenu_page(
-            $this->plugin_name . '-backend',
-            __('Add/Edit/Delete Software', 'smartmail-software-store'),
-            __('Software', 'smartmail-software-store'),
-            'manage_options',
-            $this->plugin_name . '-backend-software',
-            array($this, 'display_plugin_backend_software')
+            'smartmail-software-store-backend',
+            array(self::class, 'render_backend_page')
         );
     }
 
-    public function display_plugin_backend_dashboard() {
-        include_once 'templates/admin-backend-page.php';
-    }
-
-    public function display_plugin_backend_ebooks() {
-        include_once 'templates/admin-backend-ebooks-page.php';
-    }
-
-    public function display_plugin_backend_software() {
-        include_once 'templates/admin-backend-software-page.php';
+    public static function render_backend_page() {
+        include_once plugin_dir_path(__FILE__) . '../templates/admin-backend-page.php';
     }
 }
+
 ?>
