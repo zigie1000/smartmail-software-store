0a2f5f6b920bdef541438f77206a5bf130b1dd25
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-18 13:25:01 +0200

    Update admin-settings-page.php

--- templates/admin-settings-page.php
+++ templates/admin-settings-page.php
@@ -1,24 +1,2 @@
-<?php
-// Ensure the settings page is correctly implemented
-?>
-
-<div class="wrap">
-    <h1>SmartMail Store Settings</h1>
-    <form method="post" action="options.php">
-        <?php
-        settings_fields('smartmail_store_settings_group');
-        do_settings_sections('smartmail_store_settings_group');
-        ?>
-        <table class="form-table">
-            <tr valign="top">
-                <th scope="row">Setting 1</th>
-                <td><input type="text" name="smartmail_store_setting1" value="<?php echo esc_attr(get_option('smartmail_store_setting1')); ?>" /></td>
-            </tr>
-            <tr valign="top">
-                <th scope="row">Setting 2</th>
-                <td><input type="text" name="smartmail_store_setting2" value="<?php echo esc_attr(get_option('smartmail_store_setting2')); ?>" /></td>
-            </tr>
-        </table>
-        <?php submit_button(); ?>
-    </form>
-</div>
+<h1>SmartMail Store Settings</h1>
+<p>Configure the settings for your SmartMail Store.</p>
