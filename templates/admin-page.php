0290663677bf59d28b869de3b8d71850286d3c91
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-18 02:07:47 +0200

    Update admin-page.php

--- templates/admin-page.php
+++ templates/admin-page.php
@@ -1,29 +1,12 @@
-<?php
-/**
- * Provide a admin area view for the plugin
- *
- * This file is used to markup the admin-facing aspects of the plugin.
- *
- * @link       https://smartmail.store
- * @since      1.0.0
- *
- * @package    SmartMail_Software_Store
- * @subpackage SmartMail_Software_Store/admin/partials
- */
-?>
-
 <div class="wrap">
-    <h2><?php echo esc_html(get_admin_page_title()); ?></h2>
-    <form method="post" name="options" action="options.php">
+    <h1>SmartMail Software Store</h1>
+    <form method="post" action="options.php">
         <?php settings_fields('smartmail-software-store-settings-group'); ?>
+        <?php do_settings_sections('smartmail-software-store-settings-group'); ?>
         <table class="form-table">
             <tr valign="top">
-                <th scope="row">
-                    <label for="smartmail_software_store_settings_field">Settings field description</label>
-                </th>
-                <td>
-                    <input type="text" id="smartmail_software_store_settings_field" name="smartmail_software_store_settings_field" value="<?php echo esc_attr(get_option('smartmail_software_store_settings_field')); ?>" />
-                </td>
+            <th scope="row">Settings field description</th>
+            <td><input type="text" name="smartmail_software_store_settings_field" value="<?php echo esc_attr(get_option('smartmail_software_store_settings_field')); ?>" /></td>
             </tr>
         </table>
         <?php submit_button(); ?>
