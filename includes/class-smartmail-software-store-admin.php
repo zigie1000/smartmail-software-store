a2cb92de01b27985c7b9799449227d3e351c9575
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-18 13:00:36 +0200

    Update class-smartmail-software-store-admin.php

--- includes/class-smartmail-software-store-admin.php
+++ includes/class-smartmail-software-store-admin.php
@@ -10,11 +10,11 @@ class SmartMail_Software_Store_Admin {
     }
 
     public function enqueue_styles() {
-        wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . '../css/smartmail-software-store-admin.css', array(), $this->version, 'all');
+        wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/smartmail-software-store-admin.css', array(), $this->version, 'all');
     }
 
     public function enqueue_scripts() {
-        wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . '../js/smartmail-software-store-admin.js', array('jquery'), $this->version, false);
+        wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/smartmail-software-store-admin.js', array('jquery'), $this->version, false);
     }
 
     public function add_plugin_admin_menu() {
@@ -56,19 +56,19 @@ class SmartMail_Software_Store_Admin {
     }
 
     public function display_plugin_admin_page() {
-        include_once plugin_dir_path(__FILE__) . '../templates/admin-page.php';
+        include_once 'partials/admin-page.php';
     }
 
     public function display_settings_page() {
-        include_once plugin_dir_path(__FILE__) . '../templates/admin-settings-page.php';
+        include_once 'partials/admin-settings-page.php';
     }
 
     public function display_ebooks_page() {
-        include_once plugin_dir_path(__FILE__) . '../templates/admin-ebooks-page.php';
+        include_once 'partials/admin-ebooks-page.php';
     }
 
     public function display_software_page() {
-        include_once plugin_dir_path(__FILE__) . '../templates/admin-software-page.php';
+        include_once 'partials/admin-software-page.php';
     }
 }
 ?>
