ed1b4ccab87bd5d663d39548909ac2a74df5f7b5
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-18 02:36:15 +0200

    Update admin-page.php

--- templates/admin-page.php
+++ templates/admin-page.php
@@ -1,14 +1,12 @@
-<div class="wrap">
-    <h1>SmartMail Software Store</h1>
-    <form method="post" action="options.php">
-        <?php settings_fields('smartmail-software-store-settings-group'); ?>
-        <?php do_settings_sections('smartmail-software-store-settings-group'); ?>
-        <table class="form-table">
-            <tr valign="top">
-                <th scope="row">Settings field description</th>
-                <td><input type="text" name="smartmail_software_store_settings_field" value="<?php echo esc_attr(get_option('smartmail_software_store_settings_field')); ?>" /></td>
-            </tr>
-        </table>
-        <?php submit_button(); ?>
-    </form>
-</div>
+<?php
+// Contents for the admin-page.php template
+?>
+
+<h1>SmartMail Software Store</h1>
+<form method="post" action="options.php">
+    <?php
+    settings_fields('smartmail_software_store_options_group');
+    do_settings_sections('smartmail_software_store');
+    submit_button();
+    ?>
+</form>
