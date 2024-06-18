8b49b732411452362ef586b3382972bb6615b361
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-18 18:28:57 +0200

    Update class-smartmail-software-store-backend.php

--- includes/class-smartmail-software-store-backend.php
+++ includes/class-smartmail-software-store-backend.php
@@ -1,29 +1,27 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
-    exit; // Exit if accessed directly.
-}
-
 class SmartMail_Software_Store_Backend {
-    public function __construct() {
-        add_action('admin_menu', array($this, 'add_plugin_backend_menu'));
+    private $plugin_name;
+    private $version;
+
+    public function __construct($plugin_name, $version) {
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
     }
 
     public function add_plugin_backend_menu() {
-        add_submenu_page(
-            'smartmail-store',
-            'Software',
-            'Software',
-            'manage_options',
-            'smartmail-store-software',
-            array($this, 'display_plugin_backend_page')
+        add_menu_page(
+            'SmartMail Software Store Backend', 
+            'SmartMail Store Backend', 
+            'manage_options', 
+            $this->plugin_name . '-backend', 
+            array($this, 'display_backend_page'),
+            'dashicons-admin-generic'
         );
     }
 
-    public function display_plugin_backend_page() {
-        include_once SMARTMAIL_SOFTWARE_STORE_PLUGIN_DIR . 'includes/admin/admin-software-page.php';
+    public function display_backend_page() {
+        include_once 'partials/admin-backend-page.php';
     }
 }
-
-new SmartMail_Software_Store_Backend();
 ?>
