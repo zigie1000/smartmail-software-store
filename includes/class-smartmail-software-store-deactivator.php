fed0387db1d8f72ef4c2743485ea547ed4e9bb12
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-19 22:45:59 +0200

    Update class-smartmail-software-store-deactivator.php

--- includes/class-smartmail-software-store-deactivator.php
+++ includes/class-smartmail-software-store-deactivator.php
@@ -2,7 +2,6 @@
 
 class SmartMail_Software_Store_Deactivator {
     public static function deactivate() {
-        // Code to execute on plugin deactivation
+        // Perform tasks when the plugin is deactivated
     }
 }
-?>
