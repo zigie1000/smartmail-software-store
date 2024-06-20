a0de67d234431ed8a06fdaec0fdcb74651bd6aee
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-20 13:06:30 +0200

    Delete includes directory

--- includes/admin/admin-index.php
+++
@@ -1,4 +0,0 @@
-<div class="wrap">
-    <h1>SmartMail Software Store</h1>
-    <p>Welcome to the SmartMail Software Store plugin. Use the menu to manage eBooks and Software.</p>
-</div>
--- includes/class-smartmail-software-store-deactivator.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class SmartMail_Software_Store_Deactivator {
-    public static function deactivate() {
-        // Perform tasks when the plugin is deactivated
-    }
-}
