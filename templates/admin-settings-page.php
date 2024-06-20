69540c2fb9691ed4d2be75fc992dae15232f4555
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-20 13:26:41 +0200

    Add files via upload

---
+++ templates/admin-settings-page.php
@@ -0,0 +1,20 @@
+<div class="wrap">
+    <h1>SmartMail Software Store Settings</h1>
+    <form method="post" action="options.php">
+        <?php
+            settings_fields('smartmail_store_settings_group');
+            do_settings_sections('smartmail_store_settings_group');
+        ?>
+        <table class="form-table">
+            <tr valign="top">
+                <th scope="row">Setting 1</th>
+                <td><input type="text" name="smartmail_store_setting1" value="<?php echo esc_attr(get_option('smartmail_store_setting1')); ?>" /></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">Setting 2</th>
+                <td><input type="text" name="smartmail_store_setting2" value="<?php echo esc_attr(get_option('smartmail_store_setting2')); ?>" /></td>
+            </tr>
+        </table>
+        <?php submit_button(); ?>
+    </form>
+</div>
