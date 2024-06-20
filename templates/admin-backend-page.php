e04fa114148d6fff49677ade10f8b85e2a4f44c8
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-21 00:25:44 +0200

    Add files via upload

---
+++ templates/admin-backend-page.php
@@ -0,0 +1,10 @@
+<div class="wrap">
+    <h1><?php esc_html_e('SmartMail Software Store Backend', 'smartmail-software-store'); ?></h1>
+    <form method="post" action="options.php">
+        <?php
+        settings_fields('smartmail_software_store_backend_options_group');
+        do_settings_sections('smartmail_software_store_backend');
+        submit_button();
+        ?>
+    </form>
+</div>
