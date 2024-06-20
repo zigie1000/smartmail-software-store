5e8297d4299ef4c45c721e6fd16689562733defb
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-21 00:45:03 +0200

    Update class-smartmail-software-store.php

--- includes/class-smartmail-software-store.php
+++ includes/class-smartmail-software-store.php
@@ -118,9 +118,9 @@ class SmartMail_Software_Store {
             'menu_position' => 5,
             'show_in_admin_bar' => true,
             'show_in_nav_menus' => true,
-            'can export' => true,
+            'can_export' => true,
             'has_archive' => true,
-            'exclude_from search' => false,
+            'exclude_from_search' => false,
             'publicly_queryable' => true,
             'capability_type' => 'post',
         );
