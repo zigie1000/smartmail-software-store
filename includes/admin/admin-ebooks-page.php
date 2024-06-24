87543463fc9a94ef2147d9d664a4a6e2aa3589d2
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-24 11:05:24 +0200

    Update admin-ebooks-page.php

--- includes/admin/admin-ebooks-page.php
+++ includes/admin/admin-ebooks-page.php
@@ -10,7 +10,7 @@
     $ebooks = new WP_Query($args);
     if ($ebooks->have_posts()) {
         echo '<table class="widefat fixed" cellspacing="0">';
-        echo '<thead><tr><th class="manage-column column-title">Title</th><th class="manage-column column-author">Author</th><th class="manage-column column-date">Date</th></tr></thead>';
+        echo '<thead><tr><th class="manage-column column-title">Title</th><th class="manage-column column-author">Author</th><th class="manage-column column-date">Date</th><th class="manage-column column-thumbnail">Thumbnail</th></tr></thead>';
         echo '<tbody>';
         while ($ebooks->have_posts()) {
             $ebooks->the_post();
@@ -18,6 +18,7 @@
             echo '<td class="column-title"><strong><a class="row-title" href="' . get_edit_post_link() . '">' . get_the_title() . '</a></strong></td>';
             echo '<td class="column-author">' . get_the_author() . '</td>';
             echo '<td class="column-date">' . get_the_date() . '</td>';
+            echo '<td class="column-thumbnail">' . get_the_post_thumbnail($post->ID, 'thumbnail') . '</td>';
             echo '</tr>';
         }
         echo '</tbody>';
