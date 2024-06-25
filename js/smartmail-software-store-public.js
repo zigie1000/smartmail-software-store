6dacb1383761935a11e9c53696fbe43e787ba2f1
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-25 12:47:49 +0200

    Update smartmail-software-store-public.js

--- js/smartmail-software-store-public.js
+++ js/smartmail-software-store-public.js
@@ -2,7 +2,7 @@
 jQuery(document).ready(function($) {
     // Custom public scripts can be added here
     console.log("SmartMail Software Store Public JS Loaded");
-    
+
     // Example: Handle buy button click
     $('.buy-button').on('click', function() {
         alert("Thank you for your purchase!");
